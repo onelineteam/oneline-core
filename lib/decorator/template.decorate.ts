b63a8dc3f96b5336613768a0d24eb0563a9363bd
keep2zero <towaywu@gmail.com>
2020-04-05 11:38:12 +0800

    change: 捕获引擎的错误信息

--- lib/decorator/template.decorate.ts
+++ lib/decorator/template.decorate.ts
@@ -1,11 +1,11 @@
 // import 'reflect-metadata';
 import * as path from 'path';
 import * as fs from 'fs';
-import {parsePathParams} from '@oneline/utils';
+import { parsePathParams } from '@oneline/utils';
 
 class TemplateEngine {
-  errorJsonFormat:boolean = true;
-  type:string = 'application/json';
+  errorJsonFormat: boolean = true;
+  type: string = 'application/json';
   rootPath: string = path.resolve();
   viewPath: string = "view";
   options: any = {};
@@ -13,29 +13,39 @@ class TemplateEngine {
   handlebarsEngine: any = null;
   ejsEngine: any = null;
   defaultEngine: string = "handlebars";
-    
+
 
   constructor() {
 
-    
-    this.engines["handlebars"] =  (temp, context) => {
-      if(this.handlebarsEngine == null) throw new Error("请先设置你的handlebarsEngine"); 
-      const tempPath = parsePathParams([this.rootPath, this.viewPath,temp].join("/"), context); 
-      const source = fs.readFileSync(["/", tempPath].join("")).toString('utf-8');
-      return this.handlebarsEngine.compile(source)(context, this.options);
+
+    this.engines["handlebars"] = (temp, context) => {
+      if (this.handlebarsEngine == null) throw new Error("请先设置你的handlebarsEngine");
+
+      try {
+        const tempPath = parsePathParams([this.rootPath, this.viewPath, temp].join("/"), context);
+        const source = fs.readFileSync(["/", tempPath].join("")).toString('utf-8');
+        return this.handlebarsEngine.compile(source)(context, this.options);
+      } catch (err) {
+        throw err;
+      }
+
     }
 
-    this.engines.ejs = (temp, context) => { 
-      if(this.ejsEngine == null) throw new Error("请先设置你的ejsEngine");
+    this.engines.ejs = (temp, context) => {
+      if (this.ejsEngine == null) throw new Error("请先设置你的ejsEngine");
       const promises = (resolve, inject) => {
         // const source = fs.readFileSync([this.rootPath, this.viewPath,temp].join("/")).toString('utf-8');
         // const html = this.ejsEngine.render(source, context, this.options);
         // resolve(html);
-        const filePath = parsePathParams([this.rootPath, this.viewPath,temp].join("/"), context)
-        this.ejsEngine.renderFile(["/", filePath].join(""), context, this.options, (err, result) => {
-          if (err) throw err
-          resolve(result);
-        })
+        try {
+          const filePath = parsePathParams([this.rootPath, this.viewPath, temp].join("/"), context);
+          this.ejsEngine.renderFile(["/", filePath].join(""), context, this.options, (err, result) => {
+            if (err) throw err
+            resolve(result);
+          })
+        } catch (error) {
+          throw error;
+        }
       }
       return new Promise(promises);
     }
@@ -46,7 +56,7 @@ class TemplateEngine {
     return this.errorJsonFormat = isJson;
   }
 
-  setOptions(options:any) {
+  setOptions(options: any) {
     this.options = options;
   }
 
@@ -54,8 +64,8 @@ class TemplateEngine {
     this.viewPath = viewPath;
   }
 
-  addEngine(type: string, engine: (temp, context) => Promise<string>|string) {
-    this.engines[type] = engine; 
+  addEngine(type: string, engine: (temp, context) => Promise<string> | string) {
+    this.engines[type] = engine;
   }
 
   setEjsEngine(engine: any) {
@@ -67,29 +77,29 @@ class TemplateEngine {
   }
 
 }
- 
- 
+
+
 /**
  * 引擎类型。
  * ejs: render, options, 
  */
- 
+
 export const Templates = {};
 export const templateEngine = new TemplateEngine();
 
-export function templateEngineRender(engine:string, template: string, context: any) {
-  if(!templateEngine.engines[engine]) throw new Error("还未设置该引擎: " + engine);
-  return  templateEngine.engines[engine](template, context, templateEngine.viewPath);
+export function templateEngineRender(engine: string, template: string, context: any) {
+  if (!templateEngine.engines[engine]) throw new Error("还未设置该引擎: " + engine);
+  return templateEngine.engines[engine](template, context, templateEngine.viewPath);
 }
 
-export function Type(type: string, temp: string, engine:string) { 
-  return function(target:Object, methodName?: string, config?: any) { 
-    const key = target.constructor.name + "__" + methodName; 
-    Templates[key] = [type, engine, temp]; 
+export function Type(type: string, temp: string, engine: string) {
+  return function (target: Object, methodName?: string, config?: any) {
+    const key = target.constructor.name + "__" + methodName;
+    Templates[key] = [type, engine, temp];
     // return config;
   }
 }
 
 export function HtmlType(temp: string, engine: string) {
-  return Type("text/html", temp, engine||templateEngine.defaultEngine);
+  return Type("text/html", temp, engine || templateEngine.defaultEngine);
 }
